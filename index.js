e7859b8dbd19e29a5669fe5c0a5aac9f871430ca
Emilio Rosa <me@emiliort.com>
2022-06-05 21:42:25 -0400

    update

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongo = require("mongoose")
 const dbServe = app.get("dbServe")
 const dbName = app.get("dbName")
 
+console.log("database server ", dbServe)
+console.log("database name ", dbName)
+
 mongo.Promise = global.Promise
 mongo.connect(`${dbServe}/${dbName}?retryWrites=true&w=majority`).then(()=>{
     console.log("MongoDB connected OK")
